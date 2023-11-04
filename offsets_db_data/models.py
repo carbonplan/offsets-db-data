b6e6bc20862d4fdeead87dd2c97e5f672aac3420
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-11-03 18:37:37 -0700

    add pandera models for pandas dataframe validation

--- offsets_db_data/models.py
+++ offsets_db_data/models.py
@@ -1,5 +1,7 @@
 import typing
 
+import pandas as pd
+import pandera as pa
 import pydantic
 
 RegistryType = typing.Literal[
@@ -55,3 +57,55 @@ class Configuration(pydantic.BaseModel):
     retirements: ConfigItem | None
     cancellations: ConfigItem | None
     transactions: ConfigItem | None
+
+
+project_schema = pa.DataFrameSchema(
+    {
+        'protocol': pa.Column(pa.Object, nullable=True),  # Array of strings
+        'category': pa.Column(pa.Object, nullable=True),  # Array of strings
+        'retired': pa.Column(
+            pa.Int, pa.Check.greater_than_or_equal_to(0), nullable=True, coerce=True
+        ),
+        'issued': pa.Column(
+            pa.Int, pa.Check.greater_than_or_equal_to(0), nullable=True, coerce=True
+        ),
+        'project_id': pa.Column(pa.String, nullable=False),
+        'name': pa.Column(pa.String, nullable=True),
+        'registry': pa.Column(pa.String, nullable=False),
+        'proponent': pa.Column(pa.String, nullable=True),
+        'status': pa.Column(pa.String, nullable=True),
+        'country': pa.Column(pa.String, nullable=True),
+        'listed_at': pa.Column(pd.DatetimeTZDtype(tz='UTC'), nullable=True, required=False),
+        'is_compliance': pa.Column(pa.Bool, nullable=True),
+        'project_url': pa.Column(pa.String, nullable=True),
+    }
+)
+
+
+credit_schema = pa.DataFrameSchema(
+    {
+        'quantity': pa.Column(
+            pa.Int, pa.Check.greater_than_or_equal_to(0), nullable=True, coerce=True
+        ),
+        'id': pa.Column(pa.Int, nullable=False),
+        'project_id': pa.Column(pa.String, nullable=False),
+        'vintage': pa.Column(pa.Int, nullable=True, coerce=True),
+        'transaction_date': pa.Column(pd.DatetimeTZDtype(tz='UTC'), nullable=True),
+        'transaction_type': pa.Column(pa.String, nullable=True),
+    }
+)
+
+
+clip_schema = pa.DataFrameSchema(
+    {
+        'id': pa.Column(pa.Int, nullable=False),
+        'date': pa.Column(pd.DatetimeTZDtype(tz='UTC'), nullable=True),
+        'title': pa.Column(pa.String, nullable=True),
+        'url': pa.Column(pa.String, nullable=True),
+        'source': pa.Column(pa.String, nullable=True),
+        'tags': pa.Column(pa.Object, nullable=True),
+        'notes': pa.Column(pa.String, nullable=True),
+        'is_waybacked': pa.Column(pa.Bool, nullable=True),
+        'type': pa.Column(pa.String, nullable=True),
+    }
+)
