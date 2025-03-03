d36d0049f934d4a49d62bcebb0c4fb070d56d0fe
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-03-03 14:57:48 -0800

    Enhance orcli installation command to support custom download URL and improve file handling

--- offsets_db_data/openrefine.py
+++ offsets_db_data/openrefine.py
@@ -1,6 +1,8 @@
+import pathlib
 import shutil
 import subprocess
 
+import requests
 import rich.console
 import typer
 
@@ -10,6 +12,11 @@ console = rich.console.Console()
 
 @app.command()
 def install(
+    url: str = typer.Option(
+        'https://github.com/opencultureconsulting/orcli/raw/main/orcli',
+        help='The URL to download orcli from.',
+        show_default=True,
+    ),
     move: bool = typer.Option(
         False,
         help='If True, move the downloaded file to the specified destination.',
@@ -24,15 +31,26 @@ def install(
     """
     Install orcli from GitHub.
     """
+    file_path = destination if move else 'orcli'
+    abs_file_path = pathlib.Path(file_path).expanduser().resolve()
+    filename = abs_file_path.as_posix()
     # Download orcli from GitHub
-    subprocess.run(
-        ['wget', 'https://github.com/opencultureconsulting/orcli/raw/main/orcli'], check=True
-    )
+    # Download the file with streaming to handle large files.
+    response = requests.get(url, stream=True)
+    response.raise_for_status()  # Raise error if the download failed.
+
+    with open(filename, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:  # Filter out keep-alive chunks.
+                f.write(chunk)
+
     # Make the file executable
     subprocess.run(['chmod', '+x', 'orcli'], check=True)
     # Optionally move the file to the specified destination
     if move:
-        subprocess.run(['sudo', 'mv', 'orcli', destination], check=True)
+        subprocess.run(['mv', 'orcli', destination], check=True)
+
+    console.print(f'orcli installed to {filename}.')
 
 
 @app.command()
