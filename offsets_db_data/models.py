ee50162700ccb7d9adc422effe4b5848675f565a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-04-17 10:34:59 -0700

    Set 'project_type' and 'project_type_source' columns to non-nullable in project_schema

--- offsets_db_data/models.py
+++ offsets_db_data/models.py
@@ -26,8 +26,8 @@ project_schema = pa.DataFrameSchema(
     {
         'protocol': pa.Column(pa.Object, nullable=True),  # Array of strings
         'category': pa.Column(pa.String, nullable=True),
-        'project_type': pa.Column(pa.String, nullable=True),
-        'project_type_source': pa.Column(pa.String, nullable=True),
+        'project_type': pa.Column(pa.String, nullable=False),
+        'project_type_source': pa.Column(pa.String, nullable=False),
         'retired': pa.Column(
             pa.Int, pa.Check.greater_than_or_equal_to(0), nullable=True, coerce=True
         ),
