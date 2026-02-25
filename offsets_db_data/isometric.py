56e9bb88e52ee62a17365e75d3c1579e95477c12
Anderson Banihirwe <axbanihirwe@ualr.edu>
2026-02-25 14:55:49 -0800

    Add project type inference to process_cercarbono_projects and process_isometric_projects

--- offsets_db_data/isometric.py
+++ offsets_db_data/isometric.py
@@ -213,6 +213,7 @@ def process_isometric_projects(
         .harmonize_country_names()
         .harmonize_status_codes()
         .map_protocol(inverted_protocol_mapping=inverted_protocol_mapping)
+        .infer_project_type()
         .override_project_types(
             override_data_path=BERKELEY_PROJECT_TYPE_UPATH, source_str='berkeley'
         )
